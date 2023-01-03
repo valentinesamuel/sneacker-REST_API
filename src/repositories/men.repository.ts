cfef061a2bb7d9d0b864c5d90d8d6c59e0262cae
valentinesamuel <valentinesamuel2580@gmail.com>
2023-01-03 01:08:14 +0100

    FEAT: Added mongodb query command

--- src/repositories/men.repository.ts
+++ src/repositories/men.repository.ts
@@ -1,7 +1,7 @@
 // import {Product} from '../model/product.model';
 
 const fetchAllMensShoes = async () => {
-	// const products = await Product.find({});
+	// const products = await Product.find({{ gender: 'male', category:'shoes' }});
 	// return products;
 	console.log('About to enter db to get all mens shoes');
 };
--- src/repositories/shop.repository.ts
+++ src/repositories/shop.repository.ts
@@ -10,26 +10,26 @@ const fetchFilteredProducts = async (
 	_requestBody: unknown,
 	_dbQueryOrder: any
 ) => {
-	// const products = await Product.find({});
+	// const products = await Product.find(requestBody).sort(_dbQueryOrder);
 	// return products;
 	console.log('About to enter db to get products according to query params');
 };
 
 const fetchAllMensProducts = async () => {
-	// const products = await Product.find({});
+	// const products = await Product.find({ gender: 'male' });
 	// return products;
 	console.log("About to enter db to get all men's product");
 };
 
 const fetchAllWomensProducts = async () => {
-	// const products = await Product.find({});
+	// const products = await Product.find({{ gender: 'female' }});
 	// return products;
 	console.log("About to enter db to get all women's product");
 	return null;
 };
 
 const fetchAllKidsProducts = async () => {
-	// const products = await Product.find({});
+	// const products = await Product.find({{ age: 'kid' }});
 	// return products;
 	console.log('About to enter db to get all kids product');
 };
